6c97ddf5e782f185f191e7af070de32c72f0861a
Max Ramon <MaxRamonNB@Gmail.com>
2025-07-28 13:36:10 -0300

    Update Calculo_hospedagens.sql

--- Calculo_hospedagens.sql
+++ Calculo_hospedagens.sql
@@ -2,7 +2,7 @@ CALL listaClientes;
 CALL listaHospedagens;
 CALL tiposDados;
 CALL novoAluguel_22;
+CALL novoAluguel_23('10002', '1003', '8635', '2023-03-06', '2023-03-10', 600);
 
 SELECT * 
-FROM reservas
-WHERE cliente_id = "1002";
+FROM reservas;
