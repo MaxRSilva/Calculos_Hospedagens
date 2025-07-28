d06a7c94efc094a9802964a391ca3e54bdece618
Max Ramon <MaxRamonNB@Gmail.com>
2025-07-28 15:03:46 -0300

    Update Calculo_hospedagens.sql

--- Calculo_hospedagens.sql
+++ Calculo_hospedagens.sql
@@ -3,7 +3,8 @@ CALL listaHospedagens;
 CALL tiposDados;
 CALL novoAluguel_22;
 CALL novoAluguel_23('10003', '1003', '8635', '2023-03-06', '2023-03-10', 600);
+CALL novoAluguel_24('10005', '1003', '8635', '2023-03-13', '2023-03-16', 40);
 
 SELECT * 
 FROM reservas
-WHERE reserva_id = '10003';
+WHERE reserva_id = '10005';
