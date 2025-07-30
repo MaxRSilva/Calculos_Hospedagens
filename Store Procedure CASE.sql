1052fd7e0824c1fe5779a186120cd0c519113146
Max Ramon <MaxRamonNB@Gmail.com>
2025-07-30 14:32:44 -0300

    Update Store Procedure CASE.sql

--- Store Procedure CASE.sql
+++ Store Procedure CASE.sql
@@ -1,9 +1,9 @@
 USE `insight_place`;
-DROP PROCEDURE IF EXISTS `novoAluguel_33`;
+DROP PROCEDURE IF EXISTS `novoAluguel_34`;
 
 DELIMITER $$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `novoAluguel_33`
+CREATE DEFINER=`root`@`localhost` PROCEDURE `novoAluguel_34`
 (
     vAluguel VARCHAR(255),
     vClienteNome VARCHAR(255),
@@ -13,34 +13,37 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `novoAluguel_33`
     vPrecoUnitario DECIMAL(10,2)
 )
 BEGIN
-    DECLARE vCliente VARCHAR(100);
-    DECLARE vDias INT DEFAULT 0;
-    DECLARE vNumCliente INT;
-    DECLARE vPrecoTotal DECIMAL(10,2);
+    DECLARE vCliente VARCHAR(10);
+    DECLARE vDias INTEGER DEFAULT 0;
+    DECLARE vNumCliente INTEGER;
+    DECLARE VPrecoTotal DECIMAL(10,2);
     DECLARE vMensagem VARCHAR(100);
-
     DECLARE EXIT HANDLER FOR 1452
     BEGIN
         SET vMensagem = 'Problema de chave estrangeira associado a alguma entidade da base.';
         SELECT vMensagem;
     END;
-
     SET vNumCliente = (SELECT COUNT(*) FROM clientes WHERE nome = vClienteNome);
-
-    IF vNumCliente > 1 THEN 
-        SET vMensagem = 'Este cliente não pode ser incluido pelo nome';
+    CASE vNumCliente
+    WHEN 0 THEN
+        SET vMensagem = 'Este cliente não pode ser usado para incluir o aluguel porque não existe.';
         SELECT vMensagem;
-    ELSEIF vNumCliente = 0 THEN
-		SET vMensagem = 'Este cliente não pode ser incluido pelo nome, pois ele não tem cadastro ';
-        SELECT vMensagem;
-    ELSE 
-        SET vDias = DATEDIFF(vDataFinal, vDataInicio);
+    WHEN 1 THEN
+        SET vDias = (SELECT DATEDIFF (vDataFinal, vDataInicio));
         SET vPrecoTotal = vDias * vPrecoUnitario;
         SELECT cliente_id INTO vCliente FROM clientes WHERE nome = vClienteNome;
-        INSERT INTO reservas VALUES (vAluguel, vCliente, vHospedagem, vDataInicio, vDataFinal, vPrecoTotal);
+        INSERT INTO reservas VALUES (vAluguel, vCliente, vHospedagem, vDataInicio, 
+        vDataFinal, vPrecoTotal);
         SET vMensagem = 'Aluguel incluido na base com sucesso.';
         SELECT vMensagem;
-    END IF;
+    ELSE
+       SET vMensagem = 'Este cliente não pode ser usado para incluir o aluguel porque não existe.';
+       SELECT vMensagem;
+    END CASE;
 END$$
 
 DELIMITER ;
+
+
+
+CALL novoAluguel_33('10008', "Luana Moura", '8635', '2023-03-25', '2023-03-30', 40);
